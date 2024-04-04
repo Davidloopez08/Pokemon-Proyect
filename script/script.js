cc9b93456117829cbe468bb171d5b6c35bcccc0b
David <davidlopezlin08@gmail.com>
2024-04-04 12:48:41 +0200

    0.2.1

--- script/script.js
+++ script/script.js
@@ -30,7 +30,7 @@ function mostrarPokemon(url){
                                         #${response.id}
                                     </div>
                                     <div>
-                                        <h3 style="opacity: 0.7;">${response.name}</h3>
+                                        <h3 style="opacity: 0.7; text-transform: capitalize">${response.name}</h3>
                                     </div>
                                     <div">
                                     ${response.types.length > 1 ? 
@@ -148,7 +148,7 @@ document.addEventListener("DOMContentLoaded", function() {
                                                 #${response.id}
                                             </div>
                                             <div>
-                                                <h3 style="opacity: 0.7;">${response.name}</h3>
+                                                <h3 style="opacity: 0.7; text-transform: capitalize">${response.name}</h3>
                                             </div>
                                             <div>
                                                 ${response.types.map(type => `<img src="img/type/${type.type.name}.svg" class="img-type">`).join('')}
@@ -187,7 +187,7 @@ document.addEventListener("DOMContentLoaded", function() {
         
     });
 
-    // Mostrar sugerencias al hacer clic en el input
+    // Abrir sugerencias al hacer clic fuera del input
     searchInput.addEventListener('click', function() {
         autocomplete();
         const buttonSearch = document.getElementById('button-buscar-options')
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.form-container-options form');
     const pokedex = document.querySelector('#pokedex');
 
@@ -296,8 +296,89 @@ document.addEventListener('DOMContentLoaded', function() {
         pokedex.innerHTML = "";
         mostrarPokemon();
     });
+});*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.form-container-options form');
+    const pokedex = document.querySelector('#pokedex');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const searchTerm = document.querySelector('.input-options').value;
+        const url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2000`;
+
+        document.getElementById('button-back').style.display = "block";
+        document.getElementById('navigation-next').style.display = "none";
+        var divPokedex = document.getElementById('pokedexSearch');
+        divPokedex.innerHTML = "";
+        var divPokedex = document.getElementById('pokedex');
+        divPokedex.innerHTML = "";
+            
+            fetch(url)
+            .then((res) => res.json())
+            .then((response) => {
+                const data = response.results;
+
+                data.forEach(pokemon => {
+                    fetch(pokemon.url)
+                        .then((res) => res.json())
+                        .then((response) => {
+                            const types = response.types.map(type => type.type.name);
+                            if (types.includes(searchTerm)) {
+                                const htmlBody = `
+                                    <div class="${types[0]}">
+                                        <div class="pokemon-card">
+                                        <div>
+                                        ${response.sprites.front_default != null ? 
+                                            `<img src="${response.sprites.front_default}">` : 
+                                            `<img style="width: 50px; height: auto;" src="img/favicon.png">`}     
+                                        </div>
+                                            <div class="contenido-container">
+                                                <div class="id-pokemon">
+                                                    #${response.id}
+                                                </div>
+                                                <div>
+                                                    <h3 style="opacity: 0.7; text-transform: capitalize">${response.name}</h3>
+                                                </div>
+                                                <div>
+                                                    ${types.map(type => `<img src="img/type/${type}.svg" class="img-type">`).join('')}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`;
+                                const div = document.createElement('div');
+                                div.innerHTML = htmlBody;
+                                pokedex.appendChild(div);
+                            }
+                        });
+                });
+            })
+            .catch((error) => console.error("Error:", error));
+            index++;
+            
+        }
+    );
+
+    const backButton = document.createElement('button');
+    backButton.textContent = 'Back';
+    backButton.classList.add('navigation-next');
+    backButton.addEventListener('click', function() {
+        document.getElementById('navigation-next').style.display = "block";
+        document.getElementById('button-back').style.display = "none";
+        pokedex.innerHTML = "";
+        mostrarPokemon();
+    });
 });
 
 function autocomplete(event) {
     console.log("Evento keydown");
+  }
+
+  function limitarPokemon (number){
+    for (let index = 0; index < number; index++) {
+    
+        mostrarPokemon(url);
+
+    }
   }
